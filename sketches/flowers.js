eb1453d32bea4d8c6df85e43f35df9e34c8b964f
Dea Bankova <d.y.bankova@gmail.com>
2021-12-19 12:18:13 +0000

    added some sketches

--- sketches/flowers.js
+++ sketches/flowers.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const Random = require('canvas-sketch-util/random');
+const { lerp } = require('canvas-sketch-util/math')
 
 const settings = {
   dimensions: 'A4',
@@ -9,7 +10,29 @@ const settings = {
   units: 'cm'
 };
 
+const margin = 2
+const pink ='#ea9ab2' // '#ea9ab2' //'#b45575'
+const yellow = '#d8b982' // '#f2af29' //'#d8b982'
+const lightBlue =  '#8594aa' //'#6e7786'
+const colour = pink
+const accentColour = lightBlue
+const backgroundColour = '#262a32' //"#262f3d" // '#262a32'
+
+
 const sketch = () => {
+
+  const getPoints = () => {
+    const points = []
+    const numPoints = 30 
+    for (let x = 0; x < numPoints; x++) {
+      for (let y = 0; y < numPoints; y++) {
+        const u = x / (numPoints - 1)
+        const v = y / (numPoints - 1)
+        points.push([u, v])
+      }
+    }
+    return points
+  }
 
   const drawCircle = (
     centerX, centerY, radius, context, fill, amp, freq, fillBool
@@ -21,7 +44,7 @@ const sketch = () => {
   
     for (let angle = 0; angle <= 2 * Math.PI; angle = angle + step) { 
       let x = Math.cos(angle) * radius + centerX
-      let y = Math.sin(angle) * radius + centerX
+      let y = Math.sin(angle) * radius + centerY
       let noiseX = x + Random.noise1D(angle, amp, freq)
       let noiseY = y + Random.noise1D(angle, amp, freq)
       
@@ -36,27 +59,47 @@ const sketch = () => {
     }
   }
 
-  const pink = '#ea9ab2' //'#b45575'
-  const yellow = '#f2af29'
-  const lightBlue = '#6e7786'
-  const colour = pink
-  const accentColour = lightBlue
-  const backgroundColour = "#262a32" // '#262a32'
 
   return ({ context, width, height }) => {
     context.fillStyle = backgroundColour;
     context.fillRect(0, 0, width, height);
-    drawCircle(width*0.7, height*0.1, 9, context, colour, 0.5, 1, true)
-    drawCircle(width*0.7, height*0.1, 5, context, accentColour, 3, 5, true)
 
-    // drawCircle(width*0.8, height*0.9, 15, context, accentColour, 1.5, 1, true)
-    // drawCircle(width*0.8, height*0.9, 8, context, colour, 3, 3, true)
+    // draw a background shapes 
+    // We set our grid cell size, for the X and Y axis
+    let stepX = 0.2
+    let stepY = 0.2
+    context.lineWidth = 0.01
+    context.strokeStyle = '#8594aa'
+    for (let x = 0; x <= width; x = x + stepX) {
+      for (let y = 0; y <= height; y = y + stepY) {
+        // We create our 2-dimensional noise. The function takes 4 parameters instead of three: the first two are the coordinates, 
+        // and the last two are frequency and amplitude, just like in the noise1D case.
+        
+        let heightPercent = 1 - y / height
+        let noiseXY = Random.noise2D(x, y, 0.6 * heightPercent, 0.7 * heightPercent)
 
-    drawCircle(width*0.2, height*0.4, 6, context, accentColour, 1, 1, true)
-    drawCircle(width*0.2, height*0.4, 4, context, colour, 1, 1, true)
+        context.beginPath();
+        context.moveTo(x, y)
+        //context.arc(x + noiseXY, y + noiseXY, 0.1, 0, 2 * Math.PI, false)
+        context.lineTo(x + noiseXY, y + noiseXY)
+        context.stroke()
+      }
+    }
 
-    // drawCircle(width*0.5, height*0.5, 10, context, accentColour, 1.5, 1, true)
-    // drawCircle(width*0.5, height*0.5, 6, context, colour, 3, 3, true)
+
+
+
+    // drawCircle(width*0.7, height*0.1, 9, context, colour, 0.5, 1, true)
+    // drawCircle(width*0.7, height*0.1, 4, context, accentColour, 3, 5, true)
+
+    // // drawCircle(width*0.75, height*0.95, 10, context, accentColour, 2, 1, true)
+    // // drawCircle(width*0.75, height*0.95, 7, context, colour, 3, 3, true)
+
+    // drawCircle(width*0.15, height*0.47, 6, context, accentColour, 1, 1, true)
+    // drawCircle(width*0.15, height*0.47, 4, context, colour, 1, 1, true)
+
+    drawCircle(width*0.5, height*0.5, 9.5, context, accentColour, 1.5, 1, true)
+    drawCircle(width*0.5, height*0.5, 6, context, colour, 3, 3, true)
 
     
 
